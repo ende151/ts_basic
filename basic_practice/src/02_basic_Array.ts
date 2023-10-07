e31e9d4238e57e0b78236ddb48ad6231436facf2
ende151@163.com <ende151@163.com>
2023-10-07 23:06:33 +0800

    创建了数组类型  搭配基元类型的两种写法

---
+++ basic_practice/src/02_basic_Array.ts
@@ -0,0 +1,19 @@
+// let arr Array<Number> = [1,2,3];   // 泛型写法
+
+let arr: Array<number> = [1, 2, 3];  // 泛型写法
+
+// arr = ['a']   //  已经定义为数字类型的数组  是没办法修改数据类型的
+
+
+let arrNumber:Number[] = [1, 2, 3];
+let arrSting: string[] = ['1', '2', '3'];
+let arrStr:Array<string> = ['1', '2', '3'];
+
+
+// 这几种写法都可以
+
+/*
+    这几种写法都是可以的
+    都能实现数组搭配基元类型实现业务需求
+    
+*/
